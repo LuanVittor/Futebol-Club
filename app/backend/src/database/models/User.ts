09393da61d10543ab61cc28093748560dd5c9bdf
Luan Vitor <luan_vittor@hotmail.com>
2022-04-13 18:06:28 -0300

    services done

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,40 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id: number;
+  username: string;
+  role:string;
+  email: string;
+  password: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+export default User;
