b5e42b3df8c7a151fb5e1532742d9df1012498e1
Luan Vitor <luan_vittor@hotmail.com>
2022-04-13 19:18:35 -0300

    controller implemented

--- app/backend/src/Controllers/UserController.ts
+++ app/backend/src/Controllers/UserController.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from 'express';
+import UserService from '../Services/UserServices';
 import loginPayload from '../interfaces/ILoginPayload';
 
 class UserController {
-  constructor() {}
+  constructor(private userService = new UserService()) {}
 
   public Login = async (req: Request, res: Response) => {
     const { email, password } = req.body;
     const payload: loginPayload = { email, password };
-
+    const result = await this.userService.login(payload);
+    if (result.error) {
+      return res.status(result.code).json(result.error);
+    }
+    return res.status(200).json(result);
   }
 }
 
