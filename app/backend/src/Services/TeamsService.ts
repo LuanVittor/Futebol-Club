03375d6d7ad191f6755d38ca9b8538c3d8a5e948
Luan Vitor <luan_vittor@hotmail.com>
2022-04-16 15:10:03 -0300

    passing until 21

--- app/backend/src/Services/TeamsService.ts
+++ app/backend/src/Services/TeamsService.ts
@@ -5,12 +5,17 @@ export default class TeamService {
   private model = Teams;
 
   public async getTeams(): Promise<ITeams[]> {
-    const result = await this.model.findAll();
+    const result = await this.model.findAll({
+      attributes: ['id', ['team_name', 'teamName']], // dois parametros no segundo array eh equivalente ao alias (as:)
+    });
     return result;
   }
 
   public async getTeamById(id: number): Promise<ITeams | null> {
-    const result = await this.model.findOne({ where: { id } });
+    const result = await this.model.findOne({
+      where: { id },
+      attributes: ['id', ['team_name', 'teamName']],
+    });
     return result;
   }
 }
