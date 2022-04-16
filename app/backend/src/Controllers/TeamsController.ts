617ca9b2f34889613efc0f4a912409c6d7c60840
Luan Vitor <luan_vittor@hotmail.com>
2022-04-15 23:57:48 -0300

    feature: end ponint /teams and /teams/id add

--- app/backend/src/Controllers/TeamsController.ts
+++ app/backend/src/Controllers/TeamsController.ts
@@ -4,7 +4,7 @@ import TeamService from '../Services/TeamsService';
 export default class TeamController {
   constructor(private teamService = new TeamService()) {}
 
-  public getAllTeams = async (req: Request, res: Response) => {
+  public getAllTeams = async (_req: Request, res: Response) => {
     const result = await this.teamService.getTeams();
     return res.status(200).json(result);
   };
