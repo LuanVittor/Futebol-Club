5195f5a869c6b99a505a29f7fa610cab17da4c5e
Luan Vitor <luan_vittor@hotmail.com>
2022-04-18 13:36:15 -0300

    add: new filter to fineshed matches

--- app/backend/src/Controllers/MatchesController.ts
+++ app/backend/src/Controllers/MatchesController.ts
@@ -4,7 +4,15 @@ import MatchesService from '../Services/MatchesServices';
 export default class MatchesController {
   constructor(private matchsService = new MatchesService()) {}
 
-  public AllMatches = async (_req: Request, res: Response) => {
+  public AllMatches = async (req: Request, res: Response) => {
+    const progress = req.query.inProgress;
+
+    if (progress !== undefined) {
+      const progressString = String(progress);
+      const result = await this.matchsService.MatchesByProgress(progressString);
+      return res.status(200).json(result);
+    }
+
     const result = await this.matchsService.AllMatches();
     return res.status(200).json(result);
   };
--- app/backend/src/Services/MatchesServices.ts
+++ app/backend/src/Services/MatchesServices.ts
@@ -22,4 +22,23 @@ export default class MatchesService {
     });
     return result as IMatches[];
   }
+
+  public async MatchesByProgress(progress: string): Promise<IMatches[]> {
+    const result = await this.model.findAll({
+      where: { inProgress: progress === 'true' ? 1 : 0 },
+      include: [
+        {
+          model: Teams,
+          as: 'teamHome',
+          attributes: [['team_name', 'teamName']],
+        },
+        {
+          model: Teams,
+          as: 'teamAway',
+          attributes: [['team_name', 'teamName']],
+        },
+      ],
+    });
+    return result as IMatches[];
+  }
 }
