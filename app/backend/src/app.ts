6f0db75cdc6b7f1370b1911b4de543022b5d796d
Luan Vitor <luan_vittor@hotmail.com>
2022-04-18 21:26:41 -0300

    bug 27 fixed

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -22,8 +22,8 @@ class App {
     this.app.get('/teams/:id', teamController.getTeamById);
     this.app.get('/matches', matchesController.AllMatches);
     this.app.post('/matches', hasToken, matchesController.createMatch);
+    this.app.patch('/matches/:id', matchesController.editScore);
     this.app.patch('/matches/:id/finish', matchesController.endMatch);
-    this.app.patch('/match/:id');
   }
 
   private config():void {
